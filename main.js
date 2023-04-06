c4a4b499c57dd5fffdb8c1e4120bf03055ebf547
DennisSatrianiSp <sicsmanda12@gmail.com>
2023-04-06 16:59:53 +0700

    update pertama

---
+++ main.js
@@ -0,0 +1,10 @@
+function playAudio() {
+    alert("Musik di mainkan, maafin aku ada bug di tombol yang ngga bisa di solve :(")
+    var audio = document.getElementById("myAudio");
+    audio.play();
+  }  
+
+function putar() {
+    var audio1 = document.getElementById("song");
+    audio1.play();
+}  
